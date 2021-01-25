44ec687137be76b5d61cfa96d7967e72f24f0cc1
Duke Lu <2315328598@qq.com>
2021-01-25 22:33:13 +0800

    feat: add some demo

---
+++ src/main/java/com/idukelu/learn/java/java8/Demo1.java
@@ -0,0 +1,16 @@
+package com.idukelu.learn.java.java8;
+
+/**
+ * TODO: 编辑说明信息
+ *
+ * @author duke
+ * @date 2020.07.21
+ */
+public class Demo1 {
+
+    public static void main(String[] args) {
+    }
+
+//    public static boolean test() {
+//    }
+}
--- src/main/java/com/idukelu/learn/java/multithread/VolatileVisibleDemo.java
+++ src/main/java/com/idukelu/learn/java/multithread/VolatileVisibleDemo.java
@@ -5,105 +5,23 @@ package com.idukelu.learn.java.multithread;
  */
 public class VolatileVisibleDemo {
 
-    static int a = 0;
-    static int b = 0;
-    static int c = 0;
-    static int d = 0;
-    static int e = 0;
-    static int f = 0;
-    static int g = 0;
-    static int h = 0;
-    static int i = 0;
-    static int j = 0;
-    static int k = 0;
-    static int l = 0;
-    static int m = 0;
-    static int n = 0;
+    static boolean flag = true;
 
     public static void main(String[] args) throws InterruptedException {
-        while (true) {
-            Thread thread1 = new Thread(VolatileVisibleDemo::write);
-            Thread thread2 = new Thread(VolatileVisibleDemo::read);
-
-            thread1.start();
-            thread2.start();
-
-            thread1.join();
-            thread2.join();
-
-            a = 0;
-            b = 0;
-            c = 0;
-            d = 0;
-            e = 0;
-            f = 0;
-            g = 0;
-            h = 0;
-            i = 0;
-            j = 0;
-            k = 0;
-            l = 0;
-            m = 0;
-            n = 0;
-        }
+        new Thread(VolatileVisibleDemo::read).start();
+        Thread.sleep(1000);
+        new Thread(VolatileVisibleDemo::write).start();
     }
 
     static void write() {
-        a = 1;
-        b = 1;
-        c = 1;
-        d = 1;
-        e = 1;
-        f = 1;
-        g = 1;
-        h = 1;
-        i = 1;
-        j = 1;
-        k = 1;
-        l = 1;
-        m = 1;
-        n = 1;
+        flag = false;
     }
 
     static void read() {
-        if (b == 1 && a == 0) {
-            System.err.println("b=1");
+        // 读不到其他线程修改的值，从而无限循环
+        while (flag) {
+
         }
-        if (c == 1 && (a ==0 || b ==0)) {
-            System.err.println("c=1");
-        }
-        if (d == 1 && (a ==0 || b ==0 || c ==0)) {
-            System.err.println("d=1");
-        }
-        if (e == 1 && (a ==0 || b ==0 || c ==0 || d ==0)) {
-            System.err.println("e=1");
-        }
-        if (f == 1 && (a ==0 || b ==0 || c ==0 || d ==0)) {
-            System.err.println("f=1");
-        }
-        if (g == 1 && (a ==0 || b ==0 || c ==0 || d ==0 || e ==0 || f ==0)) {
-            System.err.println("g=1");
-        }
-        if (h == 1 && (a ==0 || b ==0 || c ==0 || d ==0 || e ==0 || f ==0 || g ==0 )) {
-            System.err.println("h=1");
-        }
-        if (i == 1 && (a ==0 || b ==0 || c ==0 || d ==0 || e ==0 || f ==0 || g ==0 || h ==0)) {
-            System.err.println("i=1");
-        }
-        if (j == 1 && (a ==0 || b ==0 || c ==0 || d ==0 || e ==0 || f ==0 || g ==0 || h ==0 || i ==0)) {
-            System.err.println("j=1");
-        }
-        if (k == 1 && (a ==0 || b ==0 || c ==0 || d ==0 || e ==0 || f ==0 || g ==0 || h ==0 || i ==0 || j ==0)) {
-            System.err.println("k=1");
-        }
-        if (l == 1 && (a ==0 || b ==0 || c ==0 || d ==0 || e ==0 || f ==0 || g ==0 || h ==0 || i ==0 || j ==0 || k ==0)) {
-            System.err.println("l=1");
-        }
-        if (m == 1 && (a ==0 || b ==0 || c ==0 || d ==0 || e ==0 || f ==0 || g ==0 || h ==0 || i ==0 || j ==0 || k ==0 || l ==0)) {
-            System.err.println("m=1");
-        }
-        if (n == 1 && (a ==0 || b ==0 || c ==0 || d ==0 || e ==0 || f ==0 || g ==0 || h ==0 || i ==0 || j ==0 || k ==0 || l ==0 || m ==0)) {
-            System.err.println("n=1");
-        }
+        System.err.println("read end");
     }
 }
