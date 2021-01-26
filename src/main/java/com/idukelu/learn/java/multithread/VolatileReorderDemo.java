2c2c0754143342b30ef60710b8e239b6d80ede15
duke.lu <duke.lu@ideamake.cn>
2021-01-26 15:59:21 +0800

    feat: add some demo

--- src/main/java/com/idukelu/learn/java/multithread/VolatileReorderDemo.java
+++ src/main/java/com/idukelu/learn/java/multithread/VolatileReorderDemo.java
@@ -34,21 +34,25 @@ public class VolatileReorderDemo {
             thread1.join();
             thread2.join();
 
-            a = 0;
-            b = 0;
-            c = 0;
-            d = 0;
-            e = 0;
-            f = 0;
-            g = 0;
-            h = 0;
-            i = 0;
-            j = 0;
-            k = 0;
-            l = 0;
-            m = 0;
-            n = 0;
+            restore();
         }
+    }
+
+    static void restore() {
+        a = 0;
+        b = 0;
+        c = 0;
+        d = 0;
+        e = 0;
+        f = 0;
+        g = 0;
+        h = 0;
+        i = 0;
+        j = 0;
+        k = 0;
+        l = 0;
+        m = 0;
+        n = 0;
     }
 
     static void write() {
